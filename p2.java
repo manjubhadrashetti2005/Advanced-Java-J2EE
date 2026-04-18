1d9c459f23c36aed027727562176dcbf4c6924d5
manjubhadrashetti2005 <manjubhadrashetti08@gmail.com>
2026-04-18 21:40:21 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,22 @@
+package basics;
+import java.util.*;
+
+public class p2 {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList<>();
+
+        ls.add("Orange");
+        ls.add("geen");
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        System.out.println(ls);
+
+        ls.remove(2);
+        System.out.println(ls);
+
+        ls.remove("Blue");
+        System.out.println(ls);
+    }
+}
