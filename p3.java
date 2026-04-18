ffc1a4ac39c5576006b72bf7794ed80895e29e6a
manjubhadrashetti2005 <manjubhadrashetti08@gmail.com>
2026-04-18 21:42:30 +0530

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,22 @@
+package basics;
+import java.util.*;
+
+public class p3 {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList<>();
+
+        ls.add("Orange");
+        ls.add("geen");
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        System.out.println("Before sorting:");
+        System.out.println(ls);
+
+        Collections.sort(ls);
+
+        System.out.println("After sorting:");
+        System.out.println(ls);
+    }
+}
