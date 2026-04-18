e0a406ce5fb85b4250c59f35a5c8b8c80ca98f00
manjubhadrashetti2005 <manjubhadrashetti08@gmail.com>
2026-04-18 21:55:20 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,20 @@
+package basics;
+import java.util.*;
+
+public class p8 {
+    public static void main(String[] args) {
+        LinkedList<String> ls = new LinkedList<>();
+
+        ls.add("Orange");
+        ls.add("geen");
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        System.out.println(ls);
+
+        ls.addLast("pink");
+
+        System.out.println(ls);
+    }
+}
